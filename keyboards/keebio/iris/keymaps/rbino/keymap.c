c6772070ea3eaba4ab781aefb8fb95f0ef6edbbb
Riccardo Binetti <rbino@gmx.com>
2020-11-10 17:36:05 +0100

    Use rotary encoder for mute and volume

--- keyboards/keebio/iris/keymaps/rbino/keymap.c
+++ keyboards/keebio/iris/keymaps/rbino/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
   RBN_LG_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    IT_OACC, IT_AACC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    RBN_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    IT_COMM, IT_DOT,  IT_MINS, RBN_CTL_DEL,
+    RBN_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_MPLY, KC_N,    KC_M,    IT_COMM, IT_DOT,  IT_MINS, RBN_CTL_DEL,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LALT, LOWER,   RBN_CTL_ENT,               KC_SPC,  RAISE,   KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -132,9 +132,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGUP);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_PGDN);
+            tap_code(KC_VOLD);
         }
     }
 }
